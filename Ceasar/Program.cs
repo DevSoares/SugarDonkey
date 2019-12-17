f62cf19340a58f41225b9cbee33657c085b0d22c
Luiz Soares <dev.soares@outlook.com>
2019-12-16 22:45:05 -0300

    Added 'StreamWriter' functionality

--- Ceasar/Program.cs
+++ Ceasar/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace Ceasar
 {
@@ -11,9 +12,10 @@ namespace Ceasar
             string html = string.Empty;
             string url = @"https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=";
             string token = "62d7bc55dd38ba9f47a64c310afb88d3dba8453e";
+            string fileName = "answer.json";
+            string targetPath = @"C:\TEMP\";
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url+token);
-            //request.AutomaticDecompression = DecompressionMethods.GZip;
 
             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
             using (Stream stream = response.GetResponseStream())
@@ -22,7 +24,11 @@ namespace Ceasar
                 html = reader.ReadToEnd();
             }
 
-            Console.WriteLine(html);
+            StreamWriter streamWriter = File.AppendText(targetPath+fileName);
+            streamWriter.Write(html);
+            streamWriter.Close();
+
+            Console.WriteLine($"File copied to {targetPath}");
             Console.ReadKey();
         }
     }
