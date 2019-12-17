c05bfd8902b3c7f2ca03b5eb8b7b0dac8aa05c4e
Luiz Soares <dev.soares@outlook.com>
2019-12-16 21:49:13 -0300

    Added 'get' functionality

--- Ceasar/Program.cs
+++ Ceasar/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.IO;
 
 namespace Ceasar
 {
@@ -6,7 +8,22 @@ namespace Ceasar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string html = string.Empty;
+            string url = @"https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=";
+            string token = "62d7bc55dd38ba9f47a64c310afb88d3dba8453e";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url+token);
+            //request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(html);
+            Console.ReadKey();
         }
     }
 }
